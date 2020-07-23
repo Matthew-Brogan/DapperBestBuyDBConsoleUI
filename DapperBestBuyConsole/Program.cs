ae99e148000688dc6c67d05917fd536d3b5d835a
Matt Brogan <broganmatt11@gmail.com>
2020-07-23 11:48:35 -0400

    Add project files.

---
+++ DapperBestBuyConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DapperBestBuyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
